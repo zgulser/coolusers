d974260b0d0dcc613d9798bf098aab43b314ad4c
Zeki Gulser <zeki@backbase.com>
2019-02-07 12:10:19 +0100

    refactor: fix gateway class warnings

--- app/src/main/java/com/ing/android/coolusers/domain/usecases/UserCommands.kt
+++ app/src/main/java/com/ing/android/coolusers/domain/usecases/UserCommands.kt
@@ -8,39 +8,52 @@ import com.ing.android.coolusers.domain.objects.User
  * Desc: Use cases for the user table
  */
 
-class GetUser(val uid: String, queryParams: Map<out String, String> = HashMap(), val gateway: CommandGateway) : Command {
+class GetUser(val uid: String, val queryParams: Map<out String, String> = HashMap(), val gateway: CommandGateway) : Command {
 
     override fun execute(genericListener: GenericListener) {
         val bundle = Bundle()
         bundle.putString(USER_ID_BUNDLE_KEY, uid)
+        queryParams.entries.iterator().forEach {
+            bundle.putString(it.key, it.value)
+        }
         gateway.performTransaction(bundle, this, genericListener)
     }
 }
 
 @SuppressWarnings("showcase: not used")
-class CreateUser(val user: User, queryParams: Map<out String, String> = HashMap(), val gateway: CommandGateway) : Command {
+class CreateUser(val user: User, val queryParams: Map<out String, String> = HashMap(), val gateway: CommandGateway) : Command {
 
     override fun execute(genericListener: GenericListener) {
+        val bundle = Bundle()
+        queryParams.entries.iterator().forEach {
+            bundle.putString(it.key, it.value)
+        }
         gateway.performTransaction(Bundle(), this, genericListener)
     }
 }
 
 @SuppressWarnings("showcase: not used")
-class DeleteUser(val uid: String, queryParams: Map<out String, String> = HashMap(), val gateway: CommandGateway) : Command {
+class DeleteUser(val uid: String, val queryParams: Map<out String, String> = HashMap(), val gateway: CommandGateway) : Command {
 
     override fun execute(genericListener: GenericListener) {
         val bundle : Bundle = Bundle.EMPTY
         bundle.putString(USER_ID_BUNDLE_KEY, uid)
+        queryParams.entries.iterator().forEach {
+            bundle.putString(it.key, it.value)
+        }
         gateway.performTransaction(Bundle(), this, genericListener)
     }
 }
 
 @SuppressWarnings("showcase: not used")
-class UpdateUser(val user: User, queryParams: Map<out String, String> = HashMap(), val gateway: CommandGateway) : Command {
+class UpdateUser(val user: User, val queryParams: Map<out String, String> = HashMap(), val gateway: CommandGateway) : Command {
 
     override fun execute(genericListener: GenericListener) {
         val bundle : Bundle = Bundle.EMPTY
         bundle.putParcelable(USER_BUNDLE_KEY, user)
+        queryParams.entries.iterator().forEach {
+            bundle.putString(it.key, it.value)
+        }
         gateway.performTransaction(Bundle(), this, genericListener)
     }
 }
